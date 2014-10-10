b16fa49c8bf14f39f186009beaad83faaba36fe0
Esa Lakaniemi <esalaka@gmail.com>
2014-10-11 01:15:57 +0300

    Maybe this works better

--- vim/install.sh
+++ vim/install.sh
@@ -5,7 +5,8 @@ mkdir -p $HOME/.vim/syntax
 cp syntax/coolbasic.vim $HOME/.vim/syntax
 
 echo "Cloning Vundle..."
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir -p $HOME/.vim/bundle
+git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 echo "Installing..."
 vim -c 'VundleInstall' -c 'qa'
 
