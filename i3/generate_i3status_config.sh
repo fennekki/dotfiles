e38c7581816c955e73f0942f4e128904f5771f45
Esa Lakaniemi <esalaka@gmail.com>
2014-10-24 15:40:01 +0300

    Now detects whether there IS a battery, or wifi
    ...or a wired ethernet interface.
    
    If you don't have one? I guess that's possible.

--- i3/generate_i3status_config.sh
+++ i3/generate_i3status_config.sh
@@ -3,9 +3,9 @@ WIRELESS_NAME="$(ip link|awk '{match($2, /(wl)[a-z0-9]*/);if (RSTART != 0) print
 WIRED_NAME="$(ip link|awk '{match($2, /(en)[a-z0-9]*/);if (RSTART != 0) print substr($2, RSTART, RLENGTH)}'|head -1)"
 BATTERY_NUM="$(ls /sys/class/power_supply/|grep BAT|head -1|sed 's/BAT\([0-9]*\)/\1/')"
 
-WIRELESS_NAME=${WIRELESS_NAME:-"wlp3s0"}
-WIRED_NAME=${WIRED_NAME:-"enp1s0"}
-BATTERY_NUM=${BATTERY_NUM:-"0"}
+WIRELESS_NAME=${WIRELESS_NAME:-"DISABLED"}
+WIRED_NAME=${WIRED_NAME:-"DISABLED"}
+BATTERY_NUM=${BATTERY_NUM:-"DISABLED"}
 
 echo '
 general {
@@ -15,11 +15,23 @@ general {
 
 order += "ipv6"
 #order += "run_watch DHCP"
-#order += "run_watch VPN"
-order += "wireless '$WIRELESS_NAME'"
-order += "ethernet '$WIRED_NAME'"
-#order += "disk /"
-order += "battery '$BATTERY_NUM'"
+#order += "run_watch VPN"'
+
+if [[ "$WIRELESS_NAME" != "DISABLED" ]]
+then
+    echo 'order += "wireless '$WIRELESS_NAME'"'
+fi
+if [[ "$WIRED_NAME" != "DISABLED" ]]
+then
+    echo 'order += "ethernet '$WIRED_NAME'"'
+fi
+
+if [[ "$BATTERY_NUM" != "DISABLED" ]]
+then
+    echo 'order += "battery '$BATTERY_NUM'"'
+fi
+
+echo '#order += "disk /"
 order += "load"
 order += "volume master"
 order += "tztime local"
