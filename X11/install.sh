176ba9907ecc23746a92ef715f35b76cbac69c42
Esa Lakaniemi <esalaka@gmail.com>
2014-12-27 00:45:44 +0200

    Yeah, let's try this

--- X11/install.sh
+++ X11/install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 mkdir -p $HOME/.fonts
-cp "Inconsolata-g for Powerline.otf" $HOME/.fonts/
+
 cp "PowerlineSymbols.otf" $HOME/.fonts/
-cp "ProggySquare-Powerline.ttf" $HOME/.fonts/
 fc-cache -fv $HOME/.fonts
