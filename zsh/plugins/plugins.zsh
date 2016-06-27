d7e1cedd8a4312f1a71893aab0987a9be553edac
Esa Lakaniemi <fennekki@iki.fi>
2016-06-27 04:42:06 +0300

    Added mix plugin for zsh

--- zsh/plugins/plugins.zsh
+++ zsh/plugins/plugins.zsh
@@ -1,2 +1,3 @@
 # This file has odd dependencies
 ZSH="$HOME/.antigen/repos/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh.git" antigen bundle git-prompt
+ZSH="$HOME/.antigen/repos/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh.git" antigen bundle mix
