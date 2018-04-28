aef7436625b445ef93fdbbb386abffd69a86cd2b
fennekki <fennekki@iki.fi>
2018-04-29 02:31:04 +0300

    Remove old environment stuff
    It was making a remote choke up.

--- sh/environment.sh
+++ sh/environment.sh
@@ -1,25 +1,4 @@
-if [ -x "$(which mlclient)" ]
-then
-    export TERMINAL="$(which mlclient)"
-elif [ -x "$(which urxvtc)" ]
-then
-    export TERMINAL="$(which urxvtc)"
-elif [ -x "$(which terminology)" ]
-then
-    export TERMINAL="$(which terminology)"
-elif [-x "$(which x-terminal-emulator)" ]
-then
-    export TERMINAL="$(which x-terminal-emulator)"
-elif [ -x "$(which uxterm)" ]
-then
-    export TERMINAL="$(which uxterm)"
-else
-    export TERMINAL="$(which xterm)"
-fi
-
-export PAGER="$(which less)"
-export EDITOR="$(which vim)"
-export PATH="$HOME/.local/bin:$HOME/.secondlife-install:$HOME/.cargo/bin:$PATH"
-export SHELL="$(which zsh)"
+export PAGER="less"
+export EDITOR="vim"
 export GPG_TTY="$(tty)"
 
