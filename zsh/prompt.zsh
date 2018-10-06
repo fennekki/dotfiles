20eccd82fea01e1460ccf0725b175e16c827414d
fennekki <fennekki@iki.fi>
2018-10-06 14:42:18 +0300

    Update prompt setting code
    Also set default tex flavour for vim's syntax recognition to "latex"
    because I don't usually do raw tex

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,17 +1,23 @@
 autoload -U colors && colors
 
-PROMPT='%{$fg_no_bold[green]%}%2~ %{$fg_bold[blue]%}%#%{%b%} '
-
-# RPROMPT should be set by plugins
-
-# Battery status for laptop
-if [[ -a /sys/class/power_supply/BAT0 ]]
+if [ "$_DOTFILES_RPROMPT_ALREADY_SET" != "yes" ]
 then
-    RPROMPT="$RPROMPT "'[`cat /sys/class/power_supply/BAT0/status` `cat /sys/class/power_supply/BAT0/capacity`%%]'
-fi
-if [[ -a /sys/class/power_supply/BAT1 ]]
-then
-    RPROMPT="$RPROMPT "'[`cat /sys/class/power_supply/BAT1/status` `cat /sys/class/power_supply/BAT1/capacity`%%]'
-fi
 
-RPROMPT="$RPROMPT "'[%{$fg_no_bold[yellow]%}%?%{$reset_color%}]'
+    PROMPT='%{$fg_no_bold[green]%}%2~ %{$fg_bold[blue]%}%#%{%b%} '
+
+    # RPROMPT should be set by plugins
+
+    # Battery status for laptop
+    if [[ -a /sys/class/power_supply/BAT0 ]]
+    then
+        RPROMPT="$RPROMPT "'[`cat /sys/class/power_supply/BAT0/status` `cat /sys/class/power_supply/BAT0/capacity`%%]'
+    fi
+    if [[ -a /sys/class/power_supply/BAT1 ]]
+    then
+        RPROMPT="$RPROMPT "'[`cat /sys/class/power_supply/BAT1/status` `cat /sys/class/power_supply/BAT1/capacity`%%]'
+    fi
+
+    RPROMPT="$RPROMPT "'[%{$fg_no_bold[yellow]%}%?%{$reset_color%}]'
+
+    export _DOTFILES_RPROMPT_ALREADY_SET="yes"
+fi
