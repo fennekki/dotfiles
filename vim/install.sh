7d30e7191990e213ec5c829842158bb7bec0325d
fennekki <fennekki@iki.fi>
2018-11-29 07:11:32 +0200

    Update vim install script to make more sense
    Also, after all these years, remove those got dang shadows from compton.

--- vim/install.sh
+++ vim/install.sh
@@ -8,24 +8,23 @@ echo "Cloning Vundle..."
 mkdir -p $HOME/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 echo "Installing..."
+
 # Run the install script and once finished, quit
 vim -c 'VundleInstall' -c 'qa'
+
+# Update plugins if they've changed, since I think install doesn't do this
+vim -c 'VundleUpdate' -c 'qa'
 
 cp ycm_extra_conf.py $HOME/.vim/ycm_extra_conf.py
 cp vimoutlinerrc $HOME/.vim/vimoutlinerrc
 
-# For YCM
-if [ ! -e $HOME/.vim/bundle/YouCompleteMe/third_party/ycmd/ycm_core.so ]
+echo "Bulding YCM"
+echo "(Requires python2.7-dev and cmake!!)"
+cd "$HOME/.vim/bundle/YouCompleteMe" &&\
+echo $PWD
+if [ "x$VIM_DEPLOY_SYSTEM_CLANG" != "x" ]
 then
-    cd "$HOME/.vim/bundle/YouCompleteMe" &&\
-    echo $PWD
-    if [ "x$VIM_DEPLOY_SYSTEM_CLANG" != "x" ]
-    then
-        python2 ./install.py --system-libclang --all
-    else
-        python2 ./install.py --all
-    fi
+    python2 ./install.py --system-libclang --all
+else
+    python2 ./install.py --all
 fi
-
-# Update plugins if they've changed
-vim -c 'VundleUpdate' -c 'qa'
