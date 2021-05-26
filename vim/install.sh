8c4121161f3de218a215c6df2f13421b6321ce1a
fennekki <fennekki@iki.fi>
2021-05-26 22:13:42 +0300

    Swap to vim-plug

--- vim/install.sh
+++ vim/install.sh
@@ -4,23 +4,14 @@ echo "Copying CoolBasic syntax..."
 mkdir -p $HOME/.vim/syntax
 cp syntax/coolbasic.vim $HOME/.vim/syntax
 
-echo "Cloning Vundle..."
-mkdir -p $HOME/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-echo "Installing..."
+echo "Installing vim-plug..."
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Run the install script and once finished, quit
-vim -c 'VundleInstall' -c 'qa'
-
-# Update plugins if they've changed, since I think install doesn't do this
-vim -c 'VundleUpdate' -c 'qa'
-
-echo "Building YCM"
-cd "$HOME/.vim/bundle/YouCompleteMe" &&\
-echo $PWD
-if [ "x$VIM_DEPLOY_SYSTEM_CLANG" != "x" ]
-then
-    python3 ./install.py --system-libclang --all
-else
-    python3 ./install.py --all
-fi
+echo "Update plugins"
+vim -c 'PlugUpgrade' -c 'PlugUpdate' -c 'qa' && (
+    if [ -d ~/.vim/bundle ]
+    then
+        echo "Removing old Vundle plugins"
+        rm -rf ~/.vim/bundle
+    fi
+)
